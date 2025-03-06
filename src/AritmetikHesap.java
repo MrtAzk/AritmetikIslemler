d7f0f58db8ffa5b607ce381da62a8d403c81a634
Mert Azko <mertazko61@gmail.com>
2025-03-07 00:30:06 +0300

    a

--- src/AritmetikHesap.java
+++ src/AritmetikHesap.java
@@ -4,13 +4,13 @@ public class AritmetikHesap {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-
+        //Değişkenler burada yapılıyor
         int a;
         int b ;
         int c ;
 
         int FinalResult ;
-
+        //Kullanıcıdan değer girilmesi isteniyor
         System.out.println("İlk Sayıyı giriniz : ");
         a =scanner.nextInt();
 
@@ -19,9 +19,9 @@ public class AritmetikHesap {
 
         System.out.println("Üçüncü Sayıyı giriniz : ");
         c =scanner.nextInt();
-
+        //İşlem önceliğine göre hesaplanıyor
         FinalResult =a+(b*c)-b;
-
+        //Sonuç giriliyor
         System.out.println(FinalResult);
 
         scanner.close();
